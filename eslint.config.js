3ed6f15ac39954fd8c21a48127cf25935d8d8fea
Aarav Malani <aarav.malani@gmail.com>
2026-04-07 15:33:04 +0800

    Add `eslint-config-prettier` and format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintConfigPrettier = require('eslint-config-prettier/flat');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintConfigPrettier,
   {
     ignores: ['dist/*'],
   },

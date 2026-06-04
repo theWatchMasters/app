ce89b107c8202023ec8336b34e95571195b89e29
Aarav Malani <aarav.malani@gmail.com>
2026-06-05 04:54:27 +0530

    chore: format files

--- components/vault/new-task.tsx
+++ components/vault/new-task.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button';
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
--- constants.ts
+++ constants.ts
@@ -1,2 +1,3 @@
 // Note: this is injected at build time
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001/';
+export const API_BASE_URL =
+  process.env.API_BASE_URL || 'http://localhost:3001/';

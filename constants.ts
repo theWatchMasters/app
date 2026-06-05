0beb64b19d930c3250e9e197524c312de88ee6d3
Aarav Malani <aarav.malani@gmail.com>
2026-06-05 05:34:01 +0530

    chore: move to EXPO_PUBLIC_ prefix

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 // Note: this is injected at build time
 export const API_BASE_URL =
-  process.env.API_BASE_URL || 'http://localhost:3001/';
+  process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:3001/';
